b364a2acd6c3b48be301e218eb56d7521529c265
20NickName20 <20nickname20@gmail.com>
2024-10-17 16:58:23 +0300

    Added target folder to gitignore and  added seperate loop for more percise simulation

--- src/main.rs
+++ src/main.rs
@@ -32,11 +32,13 @@ impl Particle {
     }
 }
 
+const PARTICLE_COUNT: usize = 2000;
+
 const WINDOW_WIDTH: u32 = 800;
 const WINDOW_HEIGHT: u32 = 800;
 
-const TIME_FACTOR: f64 = 0.0000001;
-//const TIME_FACTOR: f64 = 1.0;
+const TIME_FACTOR: f64 = 0.00025;
+const SIMULATION_TIMESTEP: f64 = 0.00000000000000000000000001;
 
 const C: f64 = 299_792_458.0;
 
@@ -60,12 +62,12 @@ fn main() {
 
     let mut particles: Vec<Particle> = Vec::new();
     let mut camera_position = Vec3::new(0.0, 0.0, 0.0);
-    let mut camera_scale = (WINDOW_HEIGHT as f64) * 1.0;
+    let mut camera_scale = (WINDOW_HEIGHT as f64) * 0.1;
 
-    for _ in 0..100 {
-        let x = rand::thread_rng().gen_range(0.0..1.0);
-        let z = rand::thread_rng().gen_range(0.0..1.0);
-        println!("x = {x}, z = {z}");
+    for _ in 0..PARTICLE_COUNT {
+        let x = rand::thread_rng().gen_range(0.0..10.0);
+        let z = rand::thread_rng().gen_range(0.0..10.0);
+        //println!("x = {x}, z = {z}");
         if rand::thread_rng().gen_bool(0.5) {
             if rand::thread_rng().gen_bool(0.5) {
                 let particle = Particle::new(Vec3::new(x, 0.0, z), PROTON_MASS, PROTON_CHARGE);
@@ -85,7 +87,7 @@ fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("universe", WINDOW_WIDTH, WINDOW_HEIGHT)
+    let window = video_subsystem.window("universe miner", WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
         .build()
         .unwrap();
@@ -94,102 +96,170 @@ fn main() {
     canvas.clear();
     canvas.present();
 
-    canvas.set_blend_mode(sdl2::render::BlendMode::Blend);
+    //canvas.set_blend_mode(sdl2::render::BlendMode::Blend);
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     let mut timer = time::SystemTime::now();
 
+    let mut is_real_time = false;
+
     println!("ready!");
     'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} => {
-                    break 'running
-                },
-                Event::KeyDown { timestamp: _, window_id: _, keycode, scancode: _, keymod: _, repeat: _ } => {
-                    if keycode != None {
-                        keyboard_map.insert(keycode.unwrap(), true);
-                    }
-                },
-                Event::KeyUp { timestamp: _, window_id: _, keycode, scancode: _, keymod: _, repeat: _ } => {
-                    if keycode != None {
-                        keyboard_map.insert(keycode.unwrap(), true);
+        if is_real_time {
+            'real_time: loop {
+                for event in event_pump.poll_iter() {
+                    match event {
+                        Event::Quit {..} => {
+                            break 'running
+                        },
+                        Event::KeyDown { timestamp: _, window_id: _, keycode, scancode: _, keymod: _, repeat: _ } => {
+
+                            if keycode != None {
+                                if keycode.unwrap() == Keycode::S {
+                                    is_real_time = !is_real_time;
+                                    break 'real_time;
+                                }
+                                keyboard_map.insert(keycode.unwrap(), true);
+                            }
+                        },
+                        Event::KeyUp { timestamp: _, window_id: _, keycode, scancode: _, keymod: _, repeat: _ } => {
+                            if keycode != None {
+                                keyboard_map.insert(keycode.unwrap(), false);
+                            }
+                        }
+                        _ => {}
                     }
                 }
-                _ => {}
-            }
-        }
-        
-        // main
-        let dt: f64 = match timer.elapsed() {
-            Ok(duration) => duration.as_nanos() as f64 / 1_000_000_000.0 * TIME_FACTOR,
-            Err(err) => {
-                println!("{}", err.to_string());
-                1.0
-            }
-        };
-        timer = time::SystemTime::now();
 
-        //println!("Fps/Tps: {}", 1.0 / dt * TIME_FACTOR);
+                // main
+                let dt: f64 = match timer.elapsed() {
+                    Ok(duration) => duration.as_nanos() as f64 / 1_000_000_000.0 * TIME_FACTOR,
+                    Err(err) => {
+                        println!("{}", err.to_string());
+                        1.0
+                    }
+                };
+                timer = time::SystemTime::now();
+    
+                for i in 0..PARTICLE_COUNT {
+                    let particle = &particles[i];
+    
+                    let mut forces = Vec3::new(0.0, 0.0, 0.0);
+    
+                    for j in 0..PARTICLE_COUNT {
+                        if i == j {continue};
+                        let particle2 = &particles[j];
+    
+                        let dist_sq = particle.pos.dist_sq(particle2.pos);
+                        let dist = dist_sq.sqrt();
 
-        let count = particles.len();
-
-        for i in 0..count {
-            let particle = &particles[i];
-
-            let mut forces = Vec3::new(0.0, 0.0, 0.0);
-
-            for j in 0..count {
-                if i == j {continue};
-                let particle2 = &particles[j];
-
-                let dist_sq = particle.pos.dist_sq(particle2.pos);
-                let dist = dist_sq.sqrt();
-                
-                let force_vec = (particle2.pos - particle.pos) / dist;
-
-                let gravitational_force_scale = G * particle.mass * particle2.mass / dist_sq;
-                let coulomb_force_scale = -K_E * particle.charge * particle2.charge / dist_sq;
-                let strong_nuclear_force_scale = /* some g const = 1 */ 1.0 * (MU + 1.0 / dist) / dist_sq * f64::consts::E.powf(-MU * dist);
-
-                let mut forces_scales: f64 = coulomb_force_scale + strong_nuclear_force_scale;
-
-                if GRAVITY_THRESHOLD_TIMES_R_OVER_MASS / dist * particle2.mass < 1.0 {
-                    forces_scales += gravitational_force_scale;
+                        let force_vec = (particle2.pos - particle.pos) / dist;
+    
+                        let gravitational_force_scale = G * particle.mass * particle2.mass / dist_sq;
+                        let coulomb_force_scale = -K_E * particle.charge * particle2.charge / dist_sq;
+                        let strong_nuclear_force_scale = /* some g const = 1 */ 1.0 * (MU + 1.0 / dist) / dist_sq * f64::consts::E.powf(-MU * dist);
+    
+                        let mut forces_scales: f64 = coulomb_force_scale + strong_nuclear_force_scale;
+    
+                        //if GRAVITY_THRESHOLD_TIMES_R_OVER_MASS / dist * particle2.mass < 1.0 {
+                            forces_scales += gravitational_force_scale;
+                        //}
+    
+                        forces += force_vec * forces_scales;
+                    }
+    
+                    let particle = &mut particles[i];
+    
+                    particle.acceleration = forces / particle.mass;
                 }
+    
+                for i in 0..PARTICLE_COUNT {
+                    let particle = &mut particles[i];
+                    particle.vel += particle.acceleration * dt;
+                    particle.pos += particle.vel * dt;
+    
+                    let draw_pos = (particle.pos - camera_position) * camera_scale;
+                    let draw_x: i32 = (draw_pos.x) as i32;
+                    let draw_y: i32 = (draw_pos.z) as i32;
+                    if particle.charge > 0.0 {
+                        canvas.set_draw_color(Color::RGBA(255, 0, 0, 255));
+                    } else if particle.charge < 0.0 {
+                        canvas.set_draw_color(Color::RGBA(0, 0, 255, 255));
+                    } else {
+                        canvas.set_draw_color(Color::RGBA(255, 255, 255, 255));
+                    }
 
-                forces += force_vec * forces_scales;
+                    canvas.fill_rect(Rect::new(draw_x, draw_y, 10, 10)).unwrap();
+                }
+    
+                canvas.present();
+                canvas.set_draw_color(Color::BLACK);
+                canvas.clear();
             }
+        } else {
+            'simulate: loop {
+                for event in event_pump.poll_iter() {
+                    match event {
+                        Event::Quit {..} => {
+                            break 'running
+                        },
+                        Event::KeyDown { timestamp: _, window_id: _, keycode, scancode: _, keymod: _, repeat: _ } => {
+                            if keycode != None {
+                                if keycode.unwrap() == Keycode::S {
+                                    is_real_time = !is_real_time;
+                                    break 'simulate;
+                                }
+                                keyboard_map.insert(keycode.unwrap(), true);
+                            }
+                        },
+                        Event::KeyUp { timestamp: _, window_id: _, keycode, scancode: _, keymod: _, repeat: _ } => {
+                            if keycode != None {
+                                keyboard_map.insert(keycode.unwrap(), false);
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+    
+                for i in 0..PARTICLE_COUNT {
+                    let particle = &particles[i];
+    
+                    let mut forces = Vec3::new(0.0, 0.0, 0.0);
+    
+                    for j in 0..PARTICLE_COUNT {
+                        if i == j {continue};
+                        let particle2 = &particles[j];
+    
+                        let dist_sq = particle.pos.dist_sq(particle2.pos);
+                        let dist = dist_sq.sqrt();
 
-            let particle = &mut particles[i];
-
-            particle.acceleration = forces / particle.mass;
-        }
-
-        for i in 0..count {
-            let particle = &mut particles[i];
-            particle.vel += particle.acceleration * dt;
-            particle.pos += particle.vel * dt;
-
-            let draw_pos = (particle.pos - camera_position) * camera_scale;
-            let draw_x: i32 = (draw_pos.x) as i32;
-            let draw_y: i32 = (draw_pos.z) as i32;
-            if particle.charge > 0.0 {
-                canvas.set_draw_color(Color::RGBA(255, 0, 0, 255));
-            } else if particle.charge < 0.0 {
-                canvas.set_draw_color(Color::RGBA(0, 0, 255, 255));
-            } else {
-                canvas.set_draw_color(Color::RGBA(255, 255, 255, 255));
+                        let force_vec = (particle2.pos - particle.pos) / dist;
+    
+                        let gravitational_force_scale = G * particle.mass * particle2.mass / dist_sq;
+                        let coulomb_force_scale = -K_E * particle.charge * particle2.charge / dist_sq;
+                        let strong_nuclear_force_scale = /* some g const = 1 */ 1.0 * (MU + 1.0 / dist) / dist_sq * f64::consts::E.powf(-MU * dist);
+    
+                        let mut forces_scales: f64 = coulomb_force_scale + strong_nuclear_force_scale;
+    
+                        //if GRAVITY_THRESHOLD_TIMES_R_OVER_MASS / dist * particle2.mass < 1.0 {
+                        //    forces_scales += gravitational_force_scale;
+                        //}
+    
+                        forces += force_vec * forces_scales;
+                    }
+    
+                    let particle = &mut particles[i];
+    
+                    particle.acceleration = forces / particle.mass;
+                }
+    
+                for i in 0..PARTICLE_COUNT {
+                    let particle = &mut particles[i];
+                    particle.vel += particle.acceleration * SIMULATION_TIMESTEP;
+                    particle.pos += particle.vel * SIMULATION_TIMESTEP;
+                }
             }
-            
-            canvas.fill_rect(Rect::new(draw_x, draw_y, 10, 10)).unwrap();
         }
-
-        canvas.present();
-
-        canvas.set_draw_color(Color::RGBA(0, 0, 0, 2));
-        canvas.fill_rect(None).unwrap();
-        //std::thread::sleep(Duration::from_secs(1))
     }
 }
